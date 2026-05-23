9c4cdc06f846720e7b2ff1c3e744215c06739f16
krusanth <krusanth.sk@gmail.com>
2026-05-23 10:33:12 +0530

    fix: fix envDir for production build, set NODE_ENV during build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  envDir: '../',
+  envDir: process.env.NODE_ENV === 'production' ? '.' : '../',
   server: {
     port: 5174,
     proxy: {
